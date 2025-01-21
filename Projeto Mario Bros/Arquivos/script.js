e31f87167b08187181f08b4f132ddb0125f8f814
Diego Ernandes <diegobass405@gmail.com>
2025-01-21 15:04:45 -0300

    Concluído

--- Projeto Mario Bros/Arquivos/script.js
+++ Projeto Mario Bros/Arquivos/script.js
@@ -1,29 +1,37 @@
-const mario = document.querySelector('.mario');//seleciona o mario
-const pipe = document.querySelector('.pipe');//seleciona o cano
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
-//função para pular
+
 const jump = () => {
     mario.classList.add('jump');
 
-    //remove a classe jump depois de 500ms
-    setTimeout(() => {//Essa função espera 500ms para executar o que está dentro dela
+    
+    setTimeout(() => {
         mario.classList.remove('jump');
     }, 500);
 }
 
-const loop = setInterval(() => {//função para verificar se o mario colidiu com o cano
-    const pipePosition = pipe.offsetLeft;//pega a posição do cano
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ' ');//pega a posição do mario no momento do click, ou seja, pulo.
-    //explicação de window.getComputedStyle(mario).bottom.replace('px', ' '): 
-    //window.getComputedStyle(mario) pega o estilo do mario, bottom pega a posição do mario em relação ao bottom, replace('px', ' ') substitui o px por um espaço vazio, ou seja, remove o px.
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ' ');
 
 
-    if(pipePosition <= 120 && marioPosition <80){//verifica se o mario colidiu com o cano
-        pipe.style.animation = 'none';//remove a animação do cano
-        pipe.style.left = `${pipePosition}px`//mantém o cano na mesma posição que o mario colidiu
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+        
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`
 
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src ='../img/game-over.png';
+        mario.style.width= "75px"
+        mario.style.marginLeft= "50px"
+
+        clearInterval(loop);
     }
-},10);//a cada 10ms ele verifica se o mario colidiu com o cano
+    
+},10);
 
-document.addEventListener('keydown', jump);//adiciona o evento de click para pular
-
+document.addEventListener('keydown', jump);
